cfbed6fda57ee6772a3bc001b0e1aa7c525d3bac
jhunderdog <33382water@gmail.com>
2020-05-03 20:23:29 +0900

    admin and users

---
+++ users/admin.py
@@ -0,0 +1,16 @@
+from django.contrib import admin
+from . import models
+
+
+@admin.register(models.User)
+class CustomUserAdmin(admin.ModelAdmin):
+    """Custom User Admin"""
+
+    list_display = ("username", "email", "gender", "language", "currency", "superhost")
+    list_filter = ("language", "currency", "superhost")
+    pass
+
+
+# admin.site.register(models.User, CustomUserAdmin)
+
+# Register your models here.
