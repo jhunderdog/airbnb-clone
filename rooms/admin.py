a644a7f28ba94e924d86df89d4a621caf49d13b2
jhunderdog <33382water@gmail.com>
2020-05-05 10:39:31 +0900

    photo

--- rooms/admin.py
+++ rooms/admin.py
@@ -2,13 +2,23 @@ from django.contrib import admin
 from . import models
 
 
-@admin.register(models.Roomtype)
+@admin.register(models.Roomtype, models.Facility, models.Amenity, models.HouseRule)
 class ItemAdmin(admin.ModelAdmin):
+    """Item Admin Definition"""
+
     pass
 
 
 @admin.register(models.Room)
 class RoomAdmin(admin.ModelAdmin):
+    """Room Admin Definition"""
+
+    pass
+
+
+@admin.register(models.Photo)
+class PhotoAdmin(admin.ModelAdmin):
+    """Photo Admin Definition"""
 
     pass
 
--- rooms/models.py
+++ rooms/models.py
@@ -18,6 +18,38 @@ class AbstractItem(core_models.TimeStampedModel):
 
 
 class Roomtype(AbstractItem):
+    """ RoomType Object Definition """
+
+    class Meta:
+        verbose_name = "Room Type"
+        ordering = ["created"]
+
+    pass
+
+
+class Amenity(AbstractItem):
+    """Amenity Object Definition"""
+
+    class Meta:
+        verbose_name_plural = "Amenities"
+
+    pass
+
+
+class Facility(AbstractItem):
+    """Facility Object Definition"""
+
+    class Meta:
+        verbose_name_plural = "Facilities"
+
+    pass
+
+
+class HouseRule(AbstractItem):
+    """HouseRule Model Definition"""
+
+    class Meta:
+        verbose_name = "House Rule"
 
     pass
 
@@ -41,10 +73,22 @@ class Room(core_models.TimeStampedModel):
     instant_book = models.BooleanField(default=False)
 
     host = models.ForeignKey(user_models.User, on_delete=models.CASCADE)
-    room_type = models.ManyToManyField(Roomtype, blank=True)
+    room_type = models.ForeignKey(Roomtype, on_delete=models.SET_NULL, null=True)
+    amenities = models.ManyToManyField(Amenity, blank=True)
+    facilities = models.ManyToManyField(Facility, blank=True)
+    house_rules = models.ManyToManyField(HouseRule, blank=True)
 
     def __str__(self):
         return self.name
+
+
+class Photo(core_models.TimeStampedModel):
+    caption = models.CharField(max_length=80)
+    file = models.ImageField()
+    room = models.ForeignKey("Room", on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.caption
 
 
 # Create your models here.
