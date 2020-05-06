677d4af061be26f92b889338c79bcdaa017ed26b
jhunderdog <33382water@gmail.com>
2020-05-06 18:46:09 +0900

    Reservation Model and Admin

--- reservations/admin.py
+++ reservations/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from . import models
+
+
+@admin.register(models.Reservation)
+class ReservationAdmin(admin.ModelAdmin):
+    pass
+
 
 # Register your models here.
--- reservations/models.py
+++ reservations/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from core import models as core_models
+
+
+class Reservation(core_models.TimeStampedModel):
+
+    """ Reservation Model Definition """
+
+    status_pending = "pending"
+    status_confirmed = "confirmed"
+    status_canceled = "canceled"
+
+    status_choices = (
+        (status_pending, "pending"),
+        (status_confirmed, "confirmed"),
+        (status_canceled, "canceled"),
+    )
+
+    status = models.CharField(
+        max_length=12, choices=status_choices, default=status_pending
+    )
+
+    guest = models.ForeignKey("users.User", on_delete=models.CASCADE)
+    room = models.ForeignKey("rooms.Room", on_delete=models.CASCADE)
+    check_in = models.DateField()
+    check_out = models.DateField()
+
+    def __str__(self):
+        return f"{self.room} - {self.check_in}"
+
 
 # Create your models here.
