e43eac2de71cac669472ec834b93d0682eb34825
jhunderdog <33382water@gmail.com>
2020-05-12 19:45:22 +0900

    Managers and Querysets

--- rooms/admin.py
+++ rooms/admin.py
@@ -41,7 +41,9 @@ class RoomAdmin(admin.ModelAdmin):
         "check_in",
         "check_out",
         "instant_book",
+        "count_amenities",
     )
+    ordering = ("name", "price", "bedrooms")
 
     list_filter = (
         "instant_book",
@@ -58,6 +60,12 @@ class RoomAdmin(admin.ModelAdmin):
     search_fields = ("=city", "^host__username")
 
     filter_horizontal = ("amenities", "facilities", "house_rules")
+
+    def count_amenities(self, obj):
+        return obj.amenities.count()
+        # return "Potato"
+
+    # count_amenities.short_description = "hello sexy!"
 
     pass
 
