e25a77e4a34b1093d9fc431a3636ede85df4493a
jhunderdog <33382water@gmail.com>
2020-05-13 20:26:42 +0900

    reelated_name

--- rooms/models.py
+++ rooms/models.py
@@ -75,10 +75,12 @@ class Room(core_models.TimeStampedModel):
     host = models.ForeignKey(
         user_models.User, related_name="rooms", on_delete=models.CASCADE
     )
-    room_type = models.ForeignKey(Roomtype, on_delete=models.SET_NULL, null=True)
-    amenities = models.ManyToManyField(Amenity, blank=True)
-    facilities = models.ManyToManyField(Facility, blank=True)
-    house_rules = models.ManyToManyField(HouseRule, blank=True)
+    room_type = models.ForeignKey(
+        Roomtype, related_name="rooms", on_delete=models.SET_NULL, null=True
+    )
+    amenities = models.ManyToManyField(Amenity, related_name="rooms", blank=True)
+    facilities = models.ManyToManyField(Facility, related_name="rooms", blank=True)
+    house_rules = models.ManyToManyField(HouseRule, related_name="rooms", blank=True)
 
     def __str__(self):
         return self.name
