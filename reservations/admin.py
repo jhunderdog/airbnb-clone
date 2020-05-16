ad84464de09fe8cfb65ca47d6ecf80032ffc96a3
jhunderdog <33382water@gmail.com>
2020-05-16 19:26:29 +0900

    Reservation Admin

--- reservations/admin.py
+++ reservations/admin.py
@@ -4,6 +4,9 @@ from . import models
 
 @admin.register(models.Reservation)
 class ReservationAdmin(admin.ModelAdmin):
+
+    list_display = ("room", "status", "check_in", "guest", "in_progress", "is_finished")
+    list_filter = ("status",)
     pass
 
 
