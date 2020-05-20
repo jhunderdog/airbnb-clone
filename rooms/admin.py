a5f1b32943662ae3206b400e55fa2b8a113368ca
jhunderdog <33382water@gmail.com>
2020-05-20 20:01:33 +0900

    Photo match with function

--- rooms/admin.py
+++ rooms/admin.py
@@ -85,6 +85,7 @@ class RoomAdmin(admin.ModelAdmin):
 
     def count_photos(self, obj):
         return obj.photos.count()
+        count_photos.short_description = "Photo Count"
 
     # count_amenities.short_description = "hello sexy!"
 
