948784ec0933d3a1909ad8b3b968e7ba488b8457
jhunderdog <33382water@gmail.com>
2020-05-28 19:00:48 +0900

    Class Based Room Detail

--- rooms/views.py
+++ rooms/views.py
@@ -1,7 +1,7 @@
 
 from django.http import Http404
 from django.shortcuts import render
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from . import models
 
 
@@ -17,9 +17,7 @@ class HomeView(ListView):
     context_object_name = "rooms"
 
 
-def room_detail(request, pk):
-    try:
-        room = models.Room.objects.get(pk=pk)
-        return render(request, "rooms/detail.html", {"room": room})
-    except models.Room.DoesNotExist:
-        raise Http404()
+class RoomDetail(DetailView):
+    model = models.Room
+    pk_url_kwarg = 'potato'
+    pass
