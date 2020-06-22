f36d22c0f2bf6932cfac556ec752488167bd350a
jhunderdog <33382water@gmail.com>
2020-06-22 18:58:00 +0900

    Customizing Stuff

--- users/views.py
+++ users/views.py
@@ -150,11 +150,6 @@ class UserProfileView(DetailView):
     model = models.User
     context_object_name = "user_obj"
 
-    def get_context_data(self, **kwargs):
-        context = super().get_context_data(**kwargs)
-        context["hello"] = "hello"
-        return context
-
 
 class UpdateUserView(UpdateView):
     model = models.User
@@ -172,7 +167,23 @@ class UpdateUserView(UpdateView):
     def get_object(self, queryset=None):
         return self.request.user
 
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields["birthdate"].widget.attrs = {"placeholder": "birthday"}
+        form.fields["first_name"].widget.attrs = {"placeholder": "first name"}
+        return form
+
 
 class UpdatePasswordView(PasswordChangeView):
     template_name = "users/update-password.html"
+
+    def get_form(self, form_class=None):
+        form = super().get_form(form_class=form_class)
+        form.fields["old_password"].widget.attrs = {"placeholder": "Current Password"}
+        form.fields["new_password1"].widget.attrs = {"placeholder": "New password"}
+        form.fields["new_password2"].widget.attrs = {
+            "placeholder": "Confirm new password"
+        }
+        return form
+
     pass
