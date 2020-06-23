0f2b7f11befe47638149f10f8245f26dda008f2b
jhunderdog <33382water@gmail.com>
2020-06-23 19:04:17 +0900

    Photo Grid

--- rooms/models.py
+++ rooms/models.py
@@ -106,6 +106,11 @@ class Room(core_models.TimeStampedModel):
         (photo,) = self.photos.all()[:1]
         return photo.file.url
 
+    def get_next_four_photos(self):
+        photos = self.photos.all()[1:5]
+        print(photos)
+        return photos
+
 
 class Photo(core_models.TimeStampedModel):
     caption = models.CharField(max_length=80)
